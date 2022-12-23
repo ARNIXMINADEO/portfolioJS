d3ccdd660c903438e6a8120a094c888acec363e2
ARNIXGAMER <arnix676@gmail.com>
2022-12-23 01:00:39 -0300

    feac: Add animation to menu button and change icon, start the about section

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const menu = document.getElementById("menu")
 menuIcon.addEventListener("click", ()=>{
     if (menu.style.left === "0%") {
         menu.style.left = "-100%";
+        menuIcon.src = "/assets/menu2.svg";
+        menuIcon.style.transform = "rotateZ(0)"
     }else{
         menu.style.left = "0%";
+        menuIcon.src = "/assets/letterx_83737.svg";
+        menuIcon.style.transform = "rotateZ(180deg)"
     }
 })
