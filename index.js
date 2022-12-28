4be6a979c09d1d6b60a9011cdbc77a0f0f4af9fd
ARNIXGAMER <arnix676@gmail.com>
2022-12-27 22:00:44 -0300

    Fixing styles

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const menuIcon = document.getElementById("menu-icon")
 const menuOptions = document.getElementById("menu-options")
 const menu = document.getElementById("menu")
+
+//Menu Desplegable inicio
 menuIcon.addEventListener("click", ()=>{
     if (menu.style.left === "0%") {
         menu.style.left = "-100%";
@@ -12,3 +14,38 @@ menuIcon.addEventListener("click", ()=>{
         menuIcon.style.transform = "rotateZ(180deg)"
     }
 })
+//Skills
+let skillsList = []
+let skillsContent = document.getElementById("skills-content")
+class Skills{
+    constructor(icon){
+        this.icon = icon;
+    }
+}
+
+//Skills creation
+let html = new Skills("/assets/html.svg");
+skillsList.push(html);
+let css = new Skills("/assets/css.svg");
+skillsList.push(css);
+let js = new Skills("/assets/js.svg");
+skillsList.push(js);
+let nodejs = new Skills("/assets/nodejs.svg");
+skillsList.push(nodejs);
+let git = new Skills("/assets/git.svg");
+skillsList.push(git);
+let github = new Skills("/assets/github.svg");
+skillsList.push(github);
+let bootstrap = new Skills("/assets/bootstrap.svg");
+skillsList.push(bootstrap);
+let react = new Skills("/assets/react.svg");
+skillsList.push(react);
+
+skillsList.forEach(e => {
+    let skillCard = `
+    <div class="skill-card">
+        <img src="${e.icon}" alt="" class="card-img">
+    </div>
+    `
+    skillsContent.innerHTML += skillCard; 
+});
