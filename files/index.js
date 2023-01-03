119b652f0076d1a674cbbacbdf400dece1452e69
ARNIXGAMER <arnix676@gmail.com>
2023-01-03 00:08:56 -0300

    Process: Add hover div for the skills

--- files/index.js
+++ files/index.js
@@ -32,34 +32,55 @@ skillButton.addEventListener("click", (e)=>{
 let skillsList = []
 let skillsContent = document.getElementById("skills-content")
 class Skills{
-    constructor(icon){
+    constructor(id,icon,name,level,type,impact){
+        this.id = id;
         this.icon = icon;
+        this.name = name;
+        this.level = level;
+        this.type = type;
+        this.impact = impact;
     }
 }
 
 //Skills creation
-let html = new Skills("/assets/html.svg");
-skillsList.push(html);
-let css = new Skills("/assets/css.svg");
-skillsList.push(css);
-let js = new Skills("/assets/js.svg");
-skillsList.push(js);
-let nodejs = new Skills("/assets/nodejs.svg");
-skillsList.push(nodejs);
-let git = new Skills("/assets/git.svg");
-skillsList.push(git);
-let github = new Skills("/assets/github.svg");
-skillsList.push(github);
-let bootstrap = new Skills("/assets/bootstrap.svg");
-skillsList.push(bootstrap);
-let react = new Skills("/assets/react.svg");
-skillsList.push(react);
+let html = new Skills("HTML","/assets/html.svg","Html","6","Structure","7");
+let css = new Skills("CSS","/assets/css.svg", "Css", "5", "Style", "8");
+let js = new Skills("JS","/assets/js.svg","JavaScript","6", "Funtionality", "5");
+let nodejs = new Skills("NODEJS","/assets/nodejs.svg","Node.JS","4","Funtionality", "6");
+let git = new Skills("GIT","/assets/git.svg","Git","5","Versions Control","7");
+let github = new Skills("GITHUB","/assets/github.svg","Github","5","Remote","6");
+let bootstrap = new Skills("BOOTSTRAP","/assets/bootstrap.svg","Bootstrap","5","Style/Structure");
+let react = new Skills("REACT","/assets/react.svg","React","0","Style/Funtionality", "7");
+
+skillsList.push(html,css,js,nodejs,git,github,bootstrap,react)
 
 skillsList.forEach(e => {
-    let skillCard = `
-    <div class="skill-card">
-        <img src="${e.icon}" alt="" class="card-img">
+    let skillCard = document.createElement("div");
+    let img = document.createElement("img")
+    skillCard.classList = "skill-card";
+    skillCard.id = e.id;
+    skillCard.style.position = "relative"
+    img.classList = "card-img";
+    img.id = e.id;
+    img.src = e.icon
+    skillsContent.appendChild(skillCard);
+    skillCard.appendChild(img); 
+    /*`
+    <div class="skill-card" id="${e.id}">
+        <img src="${e.icon}" id="${e.id}" alt="" class="card-img">
     </div>
-    `
-    skillsContent.innerHTML += skillCard; 
+    `*/
+    skillCard.addEventListener("click", (e)=>{
+        let card = document.createElement("div");
+        card.classList = "card-hover"
+        if(card.style.opacity === "0"){
+            e.target.appendChild(card)
+            card.style.opacity = "1";
+            console.log(skillCard.children)
+        }else{
+            skillCard.removeChild(card)
+            card.style.opacity = "0";
+        }
+        
+    })
 });
