9da91bafb2f6fb1d2a0dd74899e7a9cd1db0b817
ARNIXGAMER <arnix676@gmail.com>
2023-01-13 00:10:43 -0300

    Fixing: Skill card hover

--- files/index.js
+++ files/index.js
@@ -3,12 +3,12 @@ const menuOptions = document.getElementById("menu-options")
 const menu = document.getElementById("menu")
 
 //Menu Desplegable inicio
-menuIcon.addEventListener("click", ()=>{
+menuIcon.addEventListener("click", () => {
     if (menu.style.left === "0%") {
         menu.style.left = "-100%";
         menuIcon.src = "/assets/menu2.svg";
         menuIcon.style.transform = "rotateZ(0)"
-    }else{
+    } else {
         menu.style.left = "0%";
         menuIcon.src = "/assets/letterx_83737.svg";
         menuIcon.style.transform = "rotateZ(180deg)"
@@ -17,22 +17,23 @@ menuIcon.addEventListener("click", ()=>{
 //Skills
 let skillBtnContent = document.getElementById("btn-skill-content")
 let skillButton = document.getElementById("btn-skills")
-skillButton.addEventListener("click", (e)=>{
-    if(skillButton.textContent == "See more"){
+
+skillButton.addEventListener("click", (e) => {
+    if (skillButton.textContent == "See more") {
         skillButton.textContent = "See less";
         skillBtnContent.style.flexDirection = "column-reverse";
 
-    }else{
+    } else {
         skillButton.textContent = "See more";
         skillBtnContent.style.flexDirection = "column";
     }
-    
+
 })
 
 let skillsList = []
-let skillsContent = document.getElementById("skills-content")
-class Skills{
-    constructor(id,icon,name,level,type,impact){
+const skillsContent = document.getElementById("skills-content")
+class Skills {
+    constructor(id, icon, name, level, type, impact) {
         this.id = id;
         this.icon = icon;
         this.name = name;
@@ -42,17 +43,25 @@ class Skills{
     }
 }
 
-//Skills creation
-let html = new Skills("HTML","/assets/html.svg","Html","6","Structure","7");
-let css = new Skills("CSS","/assets/css.svg", "Css", "5", "Style", "8");
-let js = new Skills("JS","/assets/js.svg","JavaScript","6", "Funtionality", "5");
-let nodejs = new Skills("NODEJS","/assets/nodejs.svg","Node.JS","4","Funtionality", "6");
-let git = new Skills("GIT","/assets/git.svg","Git","5","Versions Control","7");
-let github = new Skills("GITHUB","/assets/github.svg","Github","5","Remote","6");
-let bootstrap = new Skills("BOOTSTRAP","/assets/bootstrap.svg","Bootstrap","5","Style/Structure");
-let react = new Skills("REACT","/assets/react.svg","React","0","Style/Funtionality", "7");
+skillsContent.addEventListener("click", (e) => {
+    let targetCard = skillsList.filter(card => card.id == e.target.id)
+    let card = document.createElement("div");
+    let content = `
 
-skillsList.push(html,css,js,nodejs,git,github,bootstrap,react)
+    `
+})
+
+//Skills creation
+let html = new Skills("HTML", "/assets/html.svg", "Html", "6", "Structure", "7");
+let css = new Skills("CSS", "/assets/css.svg", "Css", "5", "Style", "8");
+let js = new Skills("JS", "/assets/js.svg", "JavaScript", "6", "Funtionality", "5");
+let nodejs = new Skills("NODEJS", "/assets/nodejs.svg", "Node.JS", "4", "Funtionality", "6");
+let git = new Skills("GIT", "/assets/git.svg", "Git", "5", "Versions Control", "7");
+let github = new Skills("GITHUB", "/assets/github.svg", "Github", "5", "Remote", "6");
+let bootstrap = new Skills("BOOTSTRAP", "/assets/bootstrap.svg", "Bootstrap", "5", "Style/Structure");
+let react = new Skills("REACT", "/assets/react.svg", "React", "0", "Style/Funtionality", "7");
+
+skillsList.push(html, css, js, nodejs, git, github, bootstrap, react)
 
 skillsList.forEach(e => {
     let skillCard = document.createElement("div");
@@ -64,23 +73,10 @@ skillsList.forEach(e => {
     img.id = e.id;
     img.src = e.icon
     skillsContent.appendChild(skillCard);
-    skillCard.appendChild(img); 
+    skillCard.appendChild(img);
     /*`
     <div class="skill-card" id="${e.id}">
         <img src="${e.icon}" id="${e.id}" alt="" class="card-img">
     </div>
     `*/
-    skillCard.addEventListener("click", (e)=>{
-        let card = document.createElement("div");
-        card.classList = "card-hover"
-        if(card.style.opacity === "0"){
-            e.target.appendChild(card)
-            card.style.opacity = "1";
-            console.log(skillCard.children)
-        }else{
-            skillCard.removeChild(card)
-            card.style.opacity = "0";
-        }
-        
-    })
 });
